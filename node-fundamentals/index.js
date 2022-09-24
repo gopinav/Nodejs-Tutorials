2b699446a5fcc9416add90597c1e16883427311f
Vishwas <vishwas.g5@gmail.com>
2022-09-24 11:22:08 +0530

    Import export patterns

--- node-fundamentals/index.js
+++ node-fundamentals/index.js
@@ -1,7 +1,4 @@
-const SuperHero = require("./super-hero");
-const batman = new SuperHero("Batman");
-console.log(batman.getName());
-batman.setName("Bruce Wayne");
-console.log(batman.getName());
-const superman = new SuperHero("Superman");
-console.log(superman.getName());
+const math = require("./math");
+
+console.log(math.add(16, 26));
+console.log(math.subtract(16, 26));
---
+++ node-fundamentals/math.js
@@ -0,0 +1,22 @@
+const add = (a, b) => {
+  return a + b;
+};
+
+const subtract = (a, b) => {
+  return a - b;
+};
+
+module.exports = {
+  add,
+  subtract,
+};
+
+// module.exports = (a, b) => {
+//   return a + b;
+// };
+
+// exports = {
+//   add,
+//   subtract,
+// };
+// module.exports = add;
