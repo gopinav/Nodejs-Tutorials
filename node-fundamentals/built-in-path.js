77dee8a087fc191e76970ec2fd0eb833682a159d
Vishwas <vishwas.g5@gmail.com>
2022-09-25 17:46:35 +0530

    Path module

---
+++ node-fundamentals/built-in-path.js
@@ -0,0 +1,25 @@
+const path = require("path");
+
+console.log(path.basename(__filename));
+console.log(path.basename(__dirname));
+
+console.log(path.extname(__filename));
+console.log(path.extname(__dirname));
+
+console.log(path.parse(__filename));
+console.log(path.format(path.parse(__filename)));
+
+console.log(path.isAbsolute(__filename));
+console.log(path.isAbsolute("./data.json"));
+
+console.log(path.join("folder1", "folder2", "index.html"));
+console.log(path.join("/folder1", "folder2", "index.html"));
+console.log(path.join("/folder1", "//folder2", "index.html"));
+console.log(path.join("/folder1", "//folder2", "../index.html"));
+console.log(path.join(__dirname, "data.json"));
+
+console.log(path.resolve("folder1", "folder2", "index.html"));
+console.log(path.resolve("/folder1", "folder2", "index.html"));
+console.log(path.resolve("/folder1", "//folder2", "index.html"));
+console.log(path.resolve("/folder1", "//folder2", "../index.html"));
+console.log(path.resolve(__dirname, "data.json"));
