f35c9f0f054e9bf2e597d1abf379fa5aec614a29
Vishwas <vishwas.g5@gmail.com>
2022-10-04 08:32:54 +0530

    Events module

---
+++ node-fundamentals/built-in-events.js
@@ -0,0 +1,17 @@
+const EventEmitter = require("events");
+const emitter = new EventEmitter();
+
+// Register a listener
+emitter.on("order-pizza", (size, topping) => {
+  console.log(`Order received! Baking a ${size} pizza with ${topping}`);
+});
+
+// Register another listener
+emitter.on("order-pizza", (size) => {
+  if (size === "large") {
+    console.log("Serving complimentary drink");
+  }
+});
+
+// Emit an event
+emitter.emit("order-pizza", "large", "mushrooms");
