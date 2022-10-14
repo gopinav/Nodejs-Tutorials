3e22c6f530b6919e22d9ae7497aeeaf119467504
Vishwas <vishwas.g5@gmail.com>
2022-10-14 09:54:54 +0530

    fs Module

---
+++ node-fundamentals/built-in-fs.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+
+console.log("First");
+const fileContents = fs.readFileSync("./file.txt", "utf8");
+console.log(fileContents);
+
+console.log("Second");
+fs.readFile("./file.txt", "utf-8", (err, data) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
+});
+
+console.log("Third");
