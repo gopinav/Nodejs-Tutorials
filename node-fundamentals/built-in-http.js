7f758233438deef2df6130a5d7552268ed398c7c
Vishwas <vishwas.g5@gmail.com>
2022-11-09 21:48:58 +0530

    HTML template

--- node-fundamentals/built-in-http.js
+++ node-fundamentals/built-in-http.js
@@ -3,7 +3,13 @@ const fs = require("node:fs");
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/html" });
-  fs.createReadStream(__dirname + "/index.html").pipe(res);
+
+  const name = "Vishwas";
+  let html = fs.readFileSync(`${__dirname}/index.html`, "utf8");
+  html = html.replace("{{name}}", name);
+  res.end(html);
+
+  // fs.createReadStream(__dirname + "/index.html").pipe(res);
 
   /** JSON response
   res.writeHead(200, { "Content-Type": "application/json" });
