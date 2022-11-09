f37534d72f89a695e2561b1aba5e2b73b92d742d
Vishwas <vishwas.g5@gmail.com>
2022-11-09 18:41:41 +0530

    Responding with HTML

--- node-fundamentals/built-in-http.js
+++ node-fundamentals/built-in-http.js
@@ -1,8 +1,18 @@
 const http = require("node:http");
+const fs = require("node:fs");
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200);
-  res.end("Hello World");
+  res.writeHead(200, { "Content-Type": "text/html" });
+  fs.createReadStream(__dirname + "/index.html").pipe(res);
+
+  /** JSON response
+  res.writeHead(200, { "Content-Type": "application/json" });
+  const superHero = {
+    firstName: "Bruce",
+    lastName: "Wayne",
+  };
+  res.end(superHero);
+   */
 });
 
 server.listen(3000, () => {
