bedace130722e3c7addf1750258017577ba8425f
Vishwas <vishwas.g5@gmail.com>
2023-01-13 08:49:52 +0530

    Update index.js with simple server

--- node-fundamentals/index.js
+++ node-fundamentals/index.js
@@ -1,3 +1,12 @@
-const data = require("./data");
+// Simple HTTP server
 
-console.log(data);
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Hello world!");
+});
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => console.log("Server is running on port 3000"));
